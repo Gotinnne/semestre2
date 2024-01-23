da0d6c09a72c9b97c50455e86e0c8eebb5ce501f
Gotinnne <savegamegotin@gmail.com>
2024-01-23 16:53:14 +0100

    Scène couleur, Script CamUse
    amélioration de la scène pour test, mise en place d'une planche de couleur en style de gris.
    modification du Script CamUse afin de le faire fonctionner

--- 2Sem2Ann/Assets/S_CameraController.cs
+++ 2Sem2Ann/Assets/S_CameraController.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class S_CameraController : MonoBehaviour
 {
-    // Variables
     public Transform player;
     public float mouseSensitivity = 2f;
     float cameraVerticalRotation = 0f;
     void Start()
     {
-        // Lock and Hide the Cursor
+        // bloquer et cacher le curseur
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
     void Update()
     {
-        // Collect Mouse Input
-
+        // récupération des mouvements de la souris
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        // Rotate the Camera around its local X axis
-
+        // tourner la caméra dans l'axe X
         cameraVerticalRotation -= inputY;
         cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
         transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
 
-        // Rotate the Player Object and the Camera around its Y axis
-
+        // tourner le personnage dans l'axe y
         player.Rotate(Vector3.up * inputX);
+
     }
 }
--- 2Sem2Ann/Assets/S_PlayerController.cs
+++ 2Sem2Ann/Assets/S_PlayerController.cs
@@ -7,23 +7,44 @@ public class S_PlayerController : MonoBehaviour
     public float speed = 10f;
     public Vector3 direction;
     private Rigidbody rb;
+    private bool IsGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        // Obtenir la direction vers laquelle on applique la force, Raw permet de réaliser un ".normalized"
         direction = transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
-        rb.velocity = (direction * speed);
-        //transform.Translate(direction.normalized * speed);
-        //rb.AddForce(direction.normalized * speed, ForceMode.Force);
-        //if(rb =!IsGrounded)
-        {
 
+        // applique directement sur velocity la direction multiplier part la speed
+        // prévision problème: en fesant de cette manière la force de collisions du joueur est très puissante et assure des problèmes lors d'utilisation de la physique
+        rb.velocity = (direction * speed);
+
+        // permet d'appliquer le IS Grounded (sécurité au cas ou le joueur arrive sauter)
+        if (rb.gameObject.transform.position.y >= 0)
+        {
+            IsGrounded = false;
         }
+        if (rb.gameObject.transform.position.y == 0 & IsGrounded == false)
+        {
+            IsGrounded = true;
+        }
+        // permet d'appliquer une gravité si le joueur (sécurité au cas ou le joueur arrive sauter)
+        if (rb == !IsGrounded)
+        {
+            rb.AddForce(new Vector3(0, -9.14f, 0),ForceMode.Force);
+        }
+        if (rb == IsGrounded)
+        {
+            rb.AddForce(new Vector3(0, 0, 0), ForceMode.Force);
+        }
+
+        // TEST player controller non concluant
+        // direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        // transform.Translate(direction.normalized * speed);
+        // rb.AddForce(direction.normalized * speed, ForceMode.Force);
     }
 }
