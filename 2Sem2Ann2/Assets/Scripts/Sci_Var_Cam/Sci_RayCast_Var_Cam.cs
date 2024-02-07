c79eac0696dd3427f472166b5fac87f84f50fe75
Gotinnne <savegamegotin@gmail.com>
2024-02-07 15:36:55 +0100

    variations

---
+++ 2Sem2Ann2/Assets/Scripts/Sci_Var_Cam/Sci_RayCast_Var_Cam.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sci_RayCast_Var_Cam : MonoBehaviour
+{
+    Camera cam;
+    public GameObject Gb;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 1000f;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position, mousePos - transform.position, Color.yellow);
+
+        Gb.GetComponent<Rigidbody>().position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+     }
+}
